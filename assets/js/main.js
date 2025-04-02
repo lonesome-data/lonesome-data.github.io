3712aa3aebf88b7b7b5506c37b5b95d804018317
Sean F <19rwe14@gmail.com>
2025-04-02 10:30:55 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
     toggleSections.forEach(section => {
         section.addEventListener('click', function() {
-            this.parentNode.classList.toggle('open');
+            const detailsElement = this.parentNode;
+            detailsElement.classList.toggle('open');
+
+            if (detailsElement.classList.contains('open')) {
+                const contentHeight = detailsElement.querySelector('div').scrollHeight;
+                detailsElement.style.setProperty('--content-height', `${contentHeight}px`);
+            } else {
+                detailsElement.style.removeProperty('--content-height');
+            }
         });
     });
 });
